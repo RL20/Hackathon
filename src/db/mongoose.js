76298fe8cd259fb8ab4fe1f33613964fdac25901
RL20 <harel_shalom@yahoo.com>
2022-01-28 15:07:07 +0200

    harel-t

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
 const keys = require("../../config/keys");
 
-mongoose.connect(`mongodb+srv://Hackathon:${keys.CONNECT_PASS}@general.ezcy9.mongodb.net/game?retryWrites=true&w=majority`, {
-  useNewUrlParser: true,
-});
+mongoose.connect(`mongodb+srv://Hackathon:${keys.CONNECT_PASS}@general.ezcy9.mongodb.net/game?retryWrites=true&w=majority`);
