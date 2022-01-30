36f32856f792abbc1077deb745e6c9ed3c0a10b5
saar120 <saar120@gmail.com>
2022-01-30 16:58:59 +0200

    model of service

---
+++ src/scraper/service.js
@@ -0,0 +1,9 @@
+// timer function - runs replace collection every 24 hours.
+
+// const replaceCollection = async() => {
+
+// const scrapedData = await scrape();
+
+// replace old collection with new collection.
+
+// }
