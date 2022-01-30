f3e520b143e8ef6790fdf0fa2b8b995f81a164c8
saar120 <saar120@gmail.com>
2022-01-30 18:02:34 +0200

    routing

--- src/controllers/cardControllers.js
+++ src/controllers/cardControllers.js
@@ -1,7 +1,7 @@
 const Card = require("../models/card");
 
 //get all cards
-const getCrads = async (req, res) => {
+const getCards = async (req, res) => {
   try {
     const cards = await Card.find({});
     res.status(200).send(cards);
@@ -12,16 +12,17 @@ const getCrads = async (req, res) => {
 
 const get20CardsSet = async (req, res) => {
   try {
-    const mixed = shuffle(await getCrads());
-    const cradsSet = mixed.slice(0, 20);
-    res.status(200).send(cradsSet);
+    const cards = await Card.find({});
+    const mixed = shuffle(cards);
+    const cardsSet = mixed.slice(0, 20);
+    res.status(200).send(cardsSet);
   } catch (error) {
-    res.status(500).send({ error: " Internal Server Error" });
+    console.log(error);
+    res.status(500).send({ message: " Internal Server Error", error });
   }
 };
 //add cards
 const addCards = async (req, res) => {
-  console.log("jghjgs");
   const cardsList = req.body;
   console.log("cardsList", req.body);
   try {
@@ -59,26 +60,13 @@ const removeCard = async (req, res) => {
 };
 //! helper functions-------------------------------------------------------------------------------------
 function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
+  return array.sort(() => Math.random() - 0.5);
 }
 
 module.exports = {
-  getCrads,
+  getCards,
   get20CardsSet,
   addCard,
   addCards,
   removeCard,
 };
-
-//get random card
-// const getRandomCard = async (req, res) => {
-//   try {
-//     const cards = await getCrads();
-//     var card = cards[Math.floor(Math.random() * cards.length)];
-//     // var item = items[Math.floor(Math.random()*items.length)];
-
-//     res.status(200).send(card);
-//   } catch (error) {
-//     res.status(500).send({ error: " Internal Server Error" });
-//   }
-// };
--- src/routers/cardRouter.js
+++ src/routers/cardRouter.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = new express.Router();
-const { getCrads, get20CardsSet, addCards } = require("../controllers/cardControllers");
+const { getCards, get20CardsSet, addCards } = require("../controllers/cardControllers");
 
-router.get("", getCrads);
-router.post("", addCards);
+router.get("/", getCards);
+router.post("/", addCards);
 router.get("/random", get20CardsSet);
-// router.delete("/:id/", removeCard);
 
-// patch
 module.exports = router;
