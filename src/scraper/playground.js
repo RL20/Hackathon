17b7665d6e8ff7f29ef81e101dfcbdb577877723
saar120 <saar120@gmail.com>
2022-01-30 16:33:30 +0200

    scraping with origin

--- src/scraper/playground.js
+++ src/scraper/playground.js
@@ -2,8 +2,8 @@ const puppeteer = require("puppeteer-extra");
 const AdblockerPlugin = require("puppeteer-extra-plugin-adblocker");
 puppeteer.use(AdblockerPlugin());
 
-const getRecipes = async (page) => {
-  const recipes = await page.evaluate(() => {
+const getRecipes = async (page, origin) => {
+  const recipes = await page.evaluate((origin) => {
     let recipes = [];
     const recipeHolders = document.querySelectorAll("div.col-limit");
     for (let i = 0; i < recipeHolders.length; i++) {
@@ -13,18 +13,18 @@ const getRecipes = async (page) => {
       const recipeDescription = recipeHolder.querySelector("a > div > p.card-text").innerText;
       const recipeUrl = recipeHolder.querySelector("a").href;
 
-      recipes.push({ imageUrl, title, recipeUrl, recipeDescription });
+      recipes.push({ imageUrl, title, recipeUrl, recipeDescription, origin: origin });
     }
 
     return recipes;
-  });
+  }, origin);
   return recipes;
 };
 
 const scraper = async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  await page.goto("https://www.foodsdictionary.co.il/Recipes/Kitchen/39/5");
+  await page.goto("https://www.foodsdictionary.co.il/Recipes/Kitchen/39/");
   await page.setViewport({
     width: 1200,
     height: 800,
@@ -32,18 +32,23 @@ const scraper = async () => {
 
   const buttonSelector = "ul.paging-toolbar > li:last-child";
 
+  const titleSelector = "div.col-last-box > div > h4";
+
+  const currentOrigin = await page.$eval(titleSelector, (e) => e.innerText);
+
   let recipes = [];
 
   if (!(await isSelector(page, buttonSelector))) {
     // Single Page
     await scrollToBottom(page);
-    recipes = await getRecipes(page);
+    recipes = await getRecipes(page, currentOrigin);
   } else {
     // Multi Page
     while (await isNextButton(page, buttonSelector)) {
+      console.log(currentOrigin);
       await page.waitForTimeout(1000);
       await scrollToBottom(page);
-      recipes = [...recipes, ...(await getRecipes(page))];
+      recipes = [...recipes, ...(await getRecipes(page, currentOrigin))];
       page.click(buttonSelector);
       await page.waitForTimeout(1000);
     }
